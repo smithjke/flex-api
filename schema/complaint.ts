361bf78d9fd512a7184dd69cf97a26ccc1375380
Glebenko Egor <playhardegor@gmail.com>
2022-06-01 15:56:27 +0300

    refactor(schema) fix

--- schema/complaint.ts
+++ schema/complaint.ts
@@ -13,15 +13,15 @@ export type ComplaintRaw = {
 export function mapComplaint(data: Partial<Complaint>): ComplaintRaw {
   return {
     target: data.target,
-    type: data.target,
-    description: data.target,
+    type: data.type,
+    description: data.description,
   };
 }
 
 export function mapComplaintRaw(data: ComplaintRaw): Complaint {
   return {
     target: data.target ? String(data.target) : null,
-    type: data.target ? String(data.target) : null,
-    description: data.target ? String(data.target) : null,
+    type: data.type ? String(data.type) : null,
+    description: data.description ? String(data.description) : null,
   };
 }
